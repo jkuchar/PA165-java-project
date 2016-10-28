6fb93b1f99c8d4ad17299f14880500791b3f445f
jakubsarmir <jakub.sarmir@gmail.com>
2016-10-28 20:17:38 +0200

    car and carstate

---
+++ src/main/java/cz/fi/muni/pa165/Model/Entity/Car.java
@@ -0,0 +1,124 @@
+package cz.fi.muni.pa165.Model.Entity;
+
+import javax.persistence.*;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by SARMIR on 26. 10. 2016.
+ */
+
+@Entity
+public class Car {
+
+
+    /* DO NOT REMOVE! hack explanation @link http://stackoverflow.com/questions/2935826/why-does-hibernate-require-no-argument-constructor#comment9688725_2971717 */
+    protected Car(){}
+
+
+
+    public Car(UUID ID, int serialNumber, int regPlateNumber, String manufacturer, String type, int numberOfSeats, CarState carState, Date establishDate, Date discardDate) {
+        this.ID = ID;
+        this.serialNumber = serialNumber;
+        this.regPlateNumber = regPlateNumber;
+        this.manufacturer = manufacturer;
+        this.type = type;
+        this.numberOfSeats = numberOfSeats;
+        this.state = carState;
+        this.establishDate = establishDate;
+        this.discardDate = discardDate;
+    }
+
+    @Id
+    private UUID ID;
+
+    @NotNull
+    private int serialNumber;
+
+    @NotNull
+    private int regPlateNumber;
+
+    @NotNull
+    private String manufacturer;
+
+    @NotNull
+    private String type;
+
+    @NotNull
+    private int numberOfSeats;
+
+    @NotNull
+    private CarState state;
+
+    private Date establishDate;
+
+    private Date discardDate;
+
+
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setRegPlateNumber(int regPlateNumber) {
+        this.regPlateNumber = regPlateNumber;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public void setState(CarState state) {
+        this.state = state;
+    }
+
+    public void setEstablishDate(Date establishDate) {
+        this.establishDate = establishDate;
+    }
+
+    public void setDiscardDate(Date discardDate) {
+        this.discardDate = discardDate;
+    }
+
+
+
+    public UUID getID() { return ID; }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public int getRegPlateNumber() {
+        return regPlateNumber;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getNumberOfSeats() { return numberOfSeats; }
+
+    public CarState getCarState() { return state; }
+
+    public Date getEstablishDate() {
+        return establishDate;
+    }
+
+    public Date getDiscardDate() {
+        return discardDate;
+    }
+}
---
+++ src/main/java/cz/fi/muni/pa165/Model/Entity/CarState.java
@@ -0,0 +1,8 @@
+package cz.fi.muni.pa165.Model.Entity;
+
+/**
+ * Created by SARMIR on 27. 10. 2016.
+ */
+public enum CarState {
+    ESTABLISHED, AVAILABLE, SERVICING, DICARDED
+}
