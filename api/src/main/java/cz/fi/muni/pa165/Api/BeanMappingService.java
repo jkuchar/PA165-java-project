a3963603d0f29127600c1c8408cdd0a12b1ad5dd
Jan Kuchař <honza.kuchar@grifart.cz>
2016-11-23 22:13:05 +0100

    api: added Dozer and BeanMappingService

---
+++ api/src/main/java/cz/fi/muni/pa165/Api/BeanMappingService.java
@@ -0,0 +1,17 @@
+package cz.fi.muni.pa165.Api;
+
+import org.dozer.Mapper;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author jkuchar
+ */
+interface BeanMappingService {
+    <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
+
+    <T> T mapTo(Object u, Class<T> mapToClass);
+
+    Mapper getMapper();
+}
