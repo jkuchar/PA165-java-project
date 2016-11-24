4b45fb44dc62af085b4fb3839ab3eb76f06896f3
Jan Kuchař <honza.kuchar@grifart.cz>
2016-11-24 17:47:11 +0100

    introduces service configration

---
+++ service/src/main/java/cz/fi/muni/pa165/config/ServiceConfiguration.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.fi.muni.pa165.config;
+
+import cz.fi.muni.pa165.api.dto.CarDTO;
+import cz.fi.muni.pa165.api.facade.CarFacadeImpl;
+import cz.fi.muni.pa165.model.config.PersistenceApplicationContext;
+import cz.fi.muni.pa165.model.entity.Car;
+import cz.fi.muni.pa165.service.BeanMappingService;
+import cz.fi.muni.pa165.service.BeanMappingServiceImpl;
+import cz.fi.muni.pa165.service.CarServiceImpl;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author charlliz
+ */
+@Configuration
+@Import(PersistenceApplicationContext.class)
+@ComponentScan(basePackageClasses = {CarServiceImpl.class})
+public class ServiceConfiguration {
+
+}
