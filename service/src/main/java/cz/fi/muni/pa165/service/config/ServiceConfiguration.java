2473ba7a19ee3126b0080a322d8b4d37965d56c6
Jan Kuchař <honza.kuchar@grifart.cz>
2016-11-25 00:34:57 +0100

    config: authors comment

--- service/src/main/java/cz/fi/muni/pa165/service/config/ServiceConfiguration.java
+++ service/src/main/java/cz/fi/muni/pa165/service/config/ServiceConfiguration.java
@@ -5,22 +5,16 @@
  */
 package cz.fi.muni.pa165.service.config;
 
-import cz.fi.muni.pa165.api.dto.CarDTO;
 import cz.fi.muni.pa165.model.config.PersistenceApplicationContext;
 import cz.fi.muni.pa165.model.dao.CarAuditLogItemDao;
-import cz.fi.muni.pa165.model.entity.Car;
 import cz.fi.muni.pa165.service.CarAuditLogItemServiceImpl;
 import cz.fi.muni.pa165.service.facade.CarAuditLogItemFacadeImpl;
-import org.dozer.DozerBeanMapper;
-import org.dozer.Mapper;
-import org.dozer.loader.api.BeanMappingBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /**
- * @author charlliz
+ * @author jkuchar
  */
 @Configuration
 @Import({
@@ -33,24 +27,5 @@ import org.springframework.context.annotation.Import;
         CarAuditLogItemServiceImpl.class
 })
 public class ServiceConfiguration {
-
-    @Bean
-    public Mapper dozer() {
-        DozerBeanMapper dozer = new DozerBeanMapper();
-        dozer.addMapping(new DozerCustomConfig());
-        return dozer;
-    }
-
-    /**
-     * Custom config for Dozer if needed
-     *
-     * @author nguyen
-     */
-    public class DozerCustomConfig extends BeanMappingBuilder {
-        @Override
-        protected void configure() {
-            mapping(Car.class, CarDTO.class);
-        }
-    }
 
 }
