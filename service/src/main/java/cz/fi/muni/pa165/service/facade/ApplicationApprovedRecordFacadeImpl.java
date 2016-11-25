e026873835c7039b2db0dffcd98d75fb2e1261b7
rtrembecky <richardt@centrum.sk>
2016-11-25 13:52:25 +0100

    User and AppApproved facade implementation

---
+++ service/src/main/java/cz/fi/muni/pa165/service/facade/ApplicationApprovedRecordFacadeImpl.java
@@ -0,0 +1,87 @@
+package cz.fi.muni.pa165.service.facade;
+
+import cz.fi.muni.pa165.api.dto.ApplicationApprovedRecordDTO;
+import cz.fi.muni.pa165.api.facade.ApplicationApprovedRecordFacade;
+import cz.fi.muni.pa165.model.entity.ApplicationApprovedRecord;
+import cz.fi.muni.pa165.model.entity.Car;
+import cz.fi.muni.pa165.model.entity.RentApplication;
+import cz.fi.muni.pa165.model.entity.User;
+import cz.fi.muni.pa165.service.ApplicationApprovedRecordService;
+import cz.fi.muni.pa165.service.CarService;
+import cz.fi.muni.pa165.service.RentApplicationService;
+import cz.fi.muni.pa165.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author rtrembecky
+ */
+@Service
+@Transactional
+public class ApplicationApprovedRecordFacadeImpl implements ApplicationApprovedRecordFacade {
+
+    private final BeanMappingService beanMappingService;
+
+    private final ApplicationApprovedRecordService approvedRecordService;
+
+    private final UserService userService;
+
+    private final CarService carService;
+
+    private final RentApplicationService rentApplicationService;
+
+    @Autowired
+    public ApplicationApprovedRecordFacadeImpl (BeanMappingService beanMappingService,
+                                         ApplicationApprovedRecordService approvedRecordService,
+                                         UserService userService, CarService carService,
+                                         RentApplicationService rentApplicationService) {
+        this.beanMappingService = beanMappingService;
+        this.approvedRecordService = approvedRecordService;
+        this.userService = userService;
+        this.carService = carService;
+        this.rentApplicationService = rentApplicationService;
+    }
+
+    @Override
+    public UUID create(ApplicationApprovedRecordDTO r) {
+        Car car = carService.findCarById(r.getId());
+        User user = userService.findById(r.getId());
+        RentApplication rentApplication = rentApplicationService.findById(r.getRentApplicationId());
+        ApplicationApprovedRecord app = new ApplicationApprovedRecord(car, user, r.getFrom(), r.getTo(),
+                r.getComment(), rentApplication);
+        return app.getId();
+    }
+
+    @Override
+    public List<ApplicationApprovedRecordDTO> findAllRecords() {
+        return beanMappingService.mapTo(approvedRecordService.findAll(), ApplicationApprovedRecordDTO.class);
+    }
+
+    @Override
+    public ApplicationApprovedRecordDTO findRecordById(UUID id) {
+        return beanMappingService.mapTo(approvedRecordService.findById(id), ApplicationApprovedRecordDTO.class);
+    }
+
+    @Override
+    public List<ApplicationApprovedRecordDTO> findAllRecordsByCar(UUID carId) {
+        Car car = carService.findCarById(carId);
+        return beanMappingService.mapTo(approvedRecordService.findByCar(car), ApplicationApprovedRecordDTO.class);
+    }
+
+    @Override
+    public List<ApplicationApprovedRecordDTO> findAllRecordsByUser(UUID userId) {
+        User user = userService.findById(userId);
+        return beanMappingService.mapTo(approvedRecordService.findByUser(user), ApplicationApprovedRecordDTO.class);
+    }
+
+    @Override
+    public List<ApplicationApprovedRecordDTO> findAllRecordsCreatedBetween(Date from, Date to) {
+        return beanMappingService.mapTo(approvedRecordService.getRecordsCreatedBetween(from, to),
+                ApplicationApprovedRecordDTO.class);
+    }
+}
---
+++ service/src/main/java/cz/fi/muni/pa165/service/facade/UserFacadeImpl.java
@@ -0,0 +1,89 @@
+package cz.fi.muni.pa165.service.facade;
+
+import cz.fi.muni.pa165.api.dto.UserDTO;
+import cz.fi.muni.pa165.api.facade.UserFacade;
+import cz.fi.muni.pa165.model.PersonName;
+import cz.fi.muni.pa165.model.Role;
+import cz.fi.muni.pa165.model.entity.User;
+import cz.fi.muni.pa165.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author rtrembecky
+ */
+@Service
+@Transactional
+public class UserFacadeImpl implements UserFacade {
+
+    private final BeanMappingService beanMappingService;
+
+    private final UserService userService;
+
+    @Autowired
+    public UserFacadeImpl (BeanMappingService beanMappingService, UserService userService) {
+        this.beanMappingService = beanMappingService;
+        this.userService = userService;
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return beanMappingService.mapTo(userService.findAll(), UserDTO.class);
+    }
+
+    @Override
+    public UserDTO findById(UUID id) {
+        return beanMappingService.mapTo(userService.findById(id), UserDTO.class);
+    }
+
+    @Override
+    public UserDTO findByName(String firstName, String secondName) {
+        return beanMappingService.mapTo(userService.findByName(new PersonName(firstName, secondName)), UserDTO.class);
+    }
+
+    @Override
+    public UUID register(UserDTO u) {
+        PersonName personName = new PersonName(u.getFirstName(), u.getLastName());
+        Role role;
+        switch (u.getRole()) {
+            case ADMIN: role = Role.ADMIN;
+                break;
+            case MANAGER: role = Role.MANAGER;
+                break;
+            case USER: role = Role.USER;
+                break;
+            default: role = Role.USER;
+        }
+        User user = new User(personName, role, u.getEmail(), u.getCreated());
+        return user.getId();
+    }
+
+    @Override
+    public void delete(UserDTO u) {
+        User user = userService.findById(u.getId());
+        userService.delete(user);
+    }
+
+    @Override
+    public void update(UserDTO u) {
+        User user = userService.findById(u.getId());
+        user.setName(new PersonName(u.getFirstName(), u.getLastName()));
+        user.setEmail(u.getEmail());
+        Role role;
+        switch (u.getRole()) {
+            case ADMIN: role = Role.ADMIN;
+            break;
+            case MANAGER: role = Role.MANAGER;
+            break;
+            case USER: role = Role.USER;
+            break;
+            default: role = Role.USER;
+        }
+        user.setRole(role);
+        userService.update(user);
+    }
+}
