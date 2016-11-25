798bd891409459b6ac98028994591efe25c01e10
jakubsarmir <jakub.sarmir@gmail.com>
2016-11-25 19:23:20 +0100

    deleted entity dependency added as import in DTO

--- api/src/main/java/cz/fi/muni/pa165/api/dto/ReturnRecordDTO.java
+++ api/src/main/java/cz/fi/muni/pa165/api/dto/ReturnRecordDTO.java
@@ -1,7 +1,5 @@
 package cz.fi.muni.pa165.api.dto;
 
-import cz.fi.muni.pa165.model.entity.ReturnRecord;
-
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.Objects;
