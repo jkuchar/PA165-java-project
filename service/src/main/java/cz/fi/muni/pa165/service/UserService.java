189e68a6def4af87817c5a23dbb02a45d760df0c
rtrembecky <richardt@centrum.sk>
2016-11-25 11:24:56 +0100

    User and AppApproved services added

---
+++ service/src/main/java/cz/fi/muni/pa165/service/UserService.java
@@ -0,0 +1,51 @@
+package cz.fi.muni.pa165.service;
+
+import cz.fi.muni.pa165.model.PersonName;
+import cz.fi.muni.pa165.model.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author rtrembecky
+ */
+public interface UserService {
+
+    /**
+     * Retrieve a list of all users.
+     * @return list of all users
+     */
+    List<User> findAll();
+
+    /**
+     * Retrieve user with corresponding id.
+     * @param id is id of user
+     * @return user with certain id
+     */
+    User findById(UUID id);
+
+    /**
+     * Retrieve user with corresponding name.
+     * @param name is name of user
+     * @return return user with certain name
+     */
+    User findByName(PersonName name);
+
+    /**
+     * Register a new user.
+     * @param u is new user
+     */
+    void register(User u);
+
+    /**
+     * Delete user from database.
+     * @param u is user to delete
+     */
+    void delete(User u);
+
+    /**
+     * Update user information in database.
+     * @param u is user to update
+     */
+    void update(User u);
+}
---
+++ service/src/main/java/cz/fi/muni/pa165/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package cz.fi.muni.pa165.service;
+
+import cz.fi.muni.pa165.model.PersonName;
+import cz.fi.muni.pa165.model.dao.UserDao;
+import cz.fi.muni.pa165.model.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author rtrembecky
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl (UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return userDao.findById(id);
+    }
+
+    @Override
+    public User findByName(PersonName name) {
+        return userDao.findByName(name);
+    }
+
+    @Override
+    public void register(User u) {
+        userDao.create(u);
+    }
+
+    @Override
+    public void delete(User u) {
+        userDao.delete(u);
+    }
+
+    @Override
+    public void update(User u) {
+        userDao.update(u);
+    }
+}
