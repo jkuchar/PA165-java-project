735703402457df2468f0b3517365a5ddea65433b
rtrembecky <richardt@centrum.sk>
2017-01-13 22:38:41 +0100

    AppRejectedController cleanup

--- spring-mvc/src/main/java/cz/fi/muni/pa165/springmvc/controllers/ApplicationRejectedRecordController.java
+++ spring-mvc/src/main/java/cz/fi/muni/pa165/springmvc/controllers/ApplicationRejectedRecordController.java
@@ -5,25 +5,17 @@
  */
 package cz.fi.muni.pa165.springmvc.controllers;
 
-import cz.fi.muni.pa165.api.dto.ApplicationRejectedRecordDTO;
 import cz.fi.muni.pa165.api.facade.ApplicationRejectedRecordFacade;
-import static cz.fi.muni.pa165.springmvc.controllers.CarController.log;
-import java.util.UUID;
-import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 /**
  *
@@ -34,7 +26,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class ApplicationRejectedRecordController {
 
     final static Logger log = LoggerFactory.getLogger(ApplicationRejectedRecordController.class);
-
 
     @Autowired
     private ApplicationRejectedRecordFacade rejectedFacade;
@@ -64,37 +55,4 @@ public class ApplicationRejectedRecordController {
         model.addAttribute("record", rejectedFacade.findRecordById(id));
         return "rejected/view";
     }
-
-    /**
-     * Prepares an empty form.
-     *
-     * @param model data to be displayed
-     * @return JSP page
-     */
-    @RequestMapping(value = "/new", method = RequestMethod.GET)
-    public String newRecord(Model model) {
-        log.debug("new()");
-        model.addAttribute("createRecord", new ApplicationRejectedRecordDTO());
-        return "rejected/new";
-    }
-
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(@Valid @ModelAttribute("createRecord") ApplicationRejectedRecordDTO formBean, BindingResult bindingResult,
-                         Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
-        log.debug("create(formBean={})", formBean);
-        if (bindingResult.hasErrors()) {
-            for (ObjectError ge : bindingResult.getGlobalErrors()) {
-                log.trace("ObjectError: {}", ge);
-            }
-            for (FieldError fe : bindingResult.getFieldErrors()) {
-                model.addAttribute(fe.getField() + "_error", true);
-                log.trace("FieldError: {}", fe);
-            }
-            return "category/new";
-        }
-
-        UUID id = rejectedFacade.create(formBean);
-        redirectAttributes.addFlashAttribute("alert_success", "Category " + id + " was created");
-        return "redirect:" + uriBuilder.path("/category/list").toUriString();
-    }    
 }
